67a72b11964404b518ee6c5fe3da0273725558f7
sasha1637 <82914142+sasha1637@users.noreply.github.com>
2021-10-27 22:38:14 +0300

    правки

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,15 @@
 const ListCategoriesRef = document.querySelectorAll(".item");
 console.log(`В списке ${ListCategoriesRef.length} категории.`);
 
-const CategoriesArray = [...ListCategoriesRef]
-    .map(categories => `Категория: ${categories.children[0].textContent}
-Количество элементов: ${categories.children[1].children.length}`
-).join('\n');
-    console.log(CategoriesArray)
+// const CategoriesArray = [...ListCategoriesRef]
+//     .map(categories => `Категория: ${categories.children[0].textContent}
+// Количество элементов: ${categories.children[1].children.length}`
+// ).join('\n');
+//     console.log(CategoriesArray)
+// 1 таск попробуйте через первого и последнего ребенка решить.
+
+ListCategoriesRef.forEach(element => {
+  console.log(
+    `Категория: ${element.firstElementChild.textContent}, Количество элементов: ${element.lastElementChild.children.length}`,
+  );
+});
--- js/task-02.js
+++ js/task-02.js
@@ -11,11 +11,12 @@ const ingredients = [
 const ListRef = document.querySelector('#ingredients');
 
 
-const list = ingredients.reduce((str, ingredient) => str + `<li>${ingredient}</li>`, 'reduce');
-ListRef.innerHTML=list
-console.log(ListRef);
+const list = ingredients.reduce((str, ingredient) => str + `<li>${ingredient}</li>`,'');
+// ListRef.innerHTML = list
+// console.log(ListRef);
 
-
+// 2 таск необходимо через распыление, а innerHTML используйте с осторожностью
+ListRef.insertAdjacentHTML('beforebegin', list);
  
 
 
